410a123b11501b08317b19d95a9904c142c819c3
Kirk Yoder <yoder688@gmail.com>
2018-05-08 15:55:00 -0700

    nutrition log sums total calories

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 import MealPlanner from './components/MealPlanner.js';
@@ -8,11 +7,7 @@ class App extends Component {
     render() {
         return (
             <div className="App">
-                <header className="App-header">
-                    <img src={logo} className="App-logo" alt="logo" />
-                    <h1 className="App-title">Welcome to React</h1>
-                </header>
-
+                <h1>Meal Planner</h1>
                 <MealPlanner />
             </div>
       );
--- src/components/NutritionLog.js
+++ src/components/NutritionLog.js
@@ -1,27 +1,39 @@
 import React, { Component } from 'react';
 
 class NutritionLog extends Component {
+    calculateCalories() {
+        let sum = 0;
+        this.props.mealItems.forEach(element => {
+            sum += element.calories;
+        });
+
+        return sum;
+    }
+
     render() {
         // todo: handling null "this.props.mealItems"
         return (
-            <table>
-                <thead>
-                    <tr>
-                        <th>Name</th>
-                        <th>Calories</th>
-                    </tr>
-                </thead>
-                <tbody>
-                    {this.props.mealItems.map(function (meal, index) {
-                        return (
-                            <tr key={index}>
-                                <td>{meal.name}</td>
-                                <td>{meal.calories}</td>
-                            </tr>
-                        );
-                    })}
-                </tbody>
-            </table>
+            <div>
+                <table>
+                    <thead>
+                        <tr>
+                            <th>Name</th>
+                            <th>Calories</th>
+                        </tr>
+                    </thead>
+                    <tbody>
+                        {this.props.mealItems.map(function (meal, index) {
+                            return (
+                                <tr key={index}>
+                                    <td>{meal.name}</td>
+                                    <td>{meal.calories}</td>
+                                </tr>
+                            );
+                        })}
+                    </tbody>
+                </table>
+                <p>Total Calories: {this.calculateCalories()}</p>
+            </div>
         );
     }
 }
