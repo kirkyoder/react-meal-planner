9a501917df0f89adb11288cfc0feaca5906cd415
Kirk Yoder <yoder688@gmail.com>
2018-05-09 16:37:41 -0700

    simplify canned data model creation

--- src/data/Meals.js
+++ src/data/Meals.js
@@ -1,18 +1,26 @@
 import MealItemModel from '../models/MealItemModel';
 
-export default [
-    new MealItemModel({
-        name: 'Mediterranean Turkey Burger',
-        calories: 248,
-        protein: 30,
-        carbs: 6,
-        fat: 12
-    }),
-    new MealItemModel({
-        name: 'Lemon Chicken',
-        calories: 276,
-        protein: 32,
-        carbs: 1,
-        fat: 14
-    }),
-]
+const meals = [
+    ['Mediterranean Turkey Burger', 248, 30, 6, 12],
+    ['Rx Bar', 210, 12, 22, 8],
+    ['Total 2% Yogurt', 140, 20, 6, 4],
+    ['Orgain Protein', 150, 21, 15, 4],
+    ['Casein Protein', 120, 24, 3, 1],
+    ['Lemon Chicken', 276, 32, 1, 14],
+    ['Latte', 136, 7, 11, 7],
+    ['Bagel', 245, 10, 48, 1.5],
+    ['Eggs', 180, 12, 2, 14]
+];
+
+let mealItems = [];
+meals.forEach(function(meal) {
+    mealItems.push(new MealItemModel({
+        name: meal[0],
+        calories: meal[1],
+        protein: meal[2],
+        carbs: meal[3],
+        fat: meal[4]
+    }));
+});
+
+export default mealItems;
